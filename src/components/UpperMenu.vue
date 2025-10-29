fc8fece6aec915deabca2015915505b0e92f8716
Alex K <aklimovskikh@yandex.ru>
2025-10-29 13:32:54 +0300

    Update 29.10

--- src/components/UpperMenu.vue
+++ src/components/UpperMenu.vue
@@ -1,82 +1,82 @@
 <script lang="ts" setup>
-import { ref, onMounted, onUnmounted, computed } from "vue";
-import { useWindowSize } from "@vueuse/core";
-import DialogLogin from "./dialog/DialogLogin.vue";
-import DialogCall from "./dialog/DialogCall.vue";
-import { useAuthStore } from "../stores/auth.store";
-import { useRouter } from "vue-router";
-import { ElMessage } from "element-plus";
-import { useProfileStore } from "../stores/profile.store";
-import { ArrowDown, Menu, User } from "@element-plus/icons-vue";
+import { ref, onMounted, onUnmounted, computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+import DialogLogin from './dialog/DialogLogin.vue'
+import DialogCall from './dialog/DialogCall.vue'
+import { useAuthStore } from '../stores/auth.store'
+import { useRouter } from 'vue-router'
+import { ElMessage } from 'element-plus'
+import { useProfileStore } from '../stores/profile.store'
+import { ArrowDown, Menu, User } from '@element-plus/icons-vue'
 
-const activeIndex = ref("1");
+const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
+  console.log(key, keyPath)
+}
 
-const isLoginVisible = ref(false);
-const isCallVisible = ref(false);
+const isLoginVisible = ref(false)
+const isCallVisible = ref(false)
 
-const authStore = useAuthStore();
-const profileStore = useProfileStore();
-const router = useRouter();
+const authStore = useAuthStore()
+const profileStore = useProfileStore()
+const router = useRouter()
 
-let color = ref("");
-const { width } = useWindowSize();
-const isMobile = computed(() => width.value < 768);
-const isDrawerOpen = ref(false);
+let color = ref('')
+const { width } = useWindowSize()
+const isMobile = computed(() => width.value < 768)
+const isDrawerOpen = ref(false)
 
 // Check token on component mount
 onMounted(() => {
-  const rootStyles = getComputedStyle(document.documentElement);
-  color.value = rootStyles.getPropertyValue("--bgcolor").trim();
+  const rootStyles = getComputedStyle(document.documentElement)
+  color.value = rootStyles.getPropertyValue('--bgcolor').trim()
 
-  checkTokenValidity();
-  const tokenCheckInterval = setInterval(checkTokenValidity, 5 * 60 * 1000);
+  checkTokenValidity()
+  const tokenCheckInterval = setInterval(checkTokenValidity, 5 * 60 * 1000)
   onUnmounted(() => {
-    clearInterval(tokenCheckInterval);
-  });
-});
+    clearInterval(tokenCheckInterval)
+  })
+})
 
 // Function to check if JWT token is expired
 function isTokenExpired(token: string): boolean {
   try {
-    const payload = JSON.parse(atob(token.split(".")[1]));
-    const currentTime = Math.floor(Date.now() / 1000);
-    console.log("isTokenExpired", payload.exp - currentTime);
-    return payload.exp < currentTime;
+    const payload = JSON.parse(atob(token.split('.')[1]))
+    const currentTime = Math.floor(Date.now() / 1000)
+    console.log('isTokenExpired', payload.exp - currentTime)
+    return payload.exp < currentTime
   } catch (error) {
-    console.error("Error parsing token:", error);
-    return true; // Consider invalid tokens as expired
+    console.error('Error parsing token:', error)
+    return true // Consider invalid tokens as expired
   }
 }
 
 // Function to check token validity and logout if expired
 function checkTokenValidity() {
-  const token = authStore.getToken;
+  const token = authStore.getToken
   if (token && isTokenExpired(token)) {
-    console.log("Token expired, logging out...");
-    ElMessage.warning("Сессия истекла. Войдите в систему снова.");
-    authStore.clearToken();
-    router.push({ name: "home" });
+    console.log('Token expired, logging out...')
+    ElMessage.warning('Сессия истекла. Войдите в систему снова.')
+    authStore.clearToken()
+    router.push({ name: 'home' })
   }
 }
 
 function onLogout() {
-  authStore.clearToken();
-  router.push({ name: "home" });
+  authStore.clearToken()
+  router.push({ name: 'home' })
 }
 
 function onCallRequest() {
-  isCallVisible.value = true;
+  isCallVisible.value = true
 }
 
-function scrollToAbout() {
-  const aboutElement = document.getElementById("about");
-  if (aboutElement) {
-    aboutElement.scrollIntoView({ behavior: "smooth" });
-  }
-}
+// function scrollToAbout() {
+//   const aboutElement = document.getElementById("about");
+//   if (aboutElement) {
+//     aboutElement.scrollIntoView({ behavior: "smooth" });
+//   }
+// }
 </script>
 
 <template>
@@ -84,12 +84,7 @@ function scrollToAbout() {
     <el-col :offset="3" :span="18" :xs="{ span: 24, offset: 0 }">
       <el-header class="uppermenu-header">
         <div class="left-wrap">
-          <el-button
-            v-if="isMobile"
-            class="burger-btn"
-            text
-            @click="isDrawerOpen = true"
-          >
+          <el-button v-if="isMobile" class="burger-btn" text @click="isDrawerOpen = true">
             <el-icon size="26" color="#fff">
               <Menu />
             </el-icon>
@@ -97,12 +92,7 @@ function scrollToAbout() {
 
           <div v-if="!isMobile" class="menu-container">
             <!-- Красная кнопка ЦКП -->
-            <el-button
-              class="logo-btn"
-              @click="router.push({ path: '/' })"
-            >
-              Аэромакс
-            </el-button>
+            <el-button class="logo-btn" @click="router.push({ path: '/' })"> Аэромакс </el-button>
 
             <!-- Меню с выпадающим списком -->
             <el-menu
@@ -125,10 +115,7 @@ function scrollToAbout() {
                 </template>
                 <el-sub-menu index="1-1">
                   <template #title>Механообрабатывающее производство ></template>
-                  <el-menu-item
-                    index="/machining"
-                    :route="{ path: '/machining' }"
-                  >
+                  <el-menu-item index="/machining" :route="{ path: '/machining' }">
                     Токарные работы
                   </el-menu-item>
                   <el-menu-item index="/milling" :route="{ path: '/milling' }">
@@ -170,20 +157,11 @@ function scrollToAbout() {
             </el-menu>
           </div>
         </div>
-        
+
         <div class="right-wrap">
-          <el-button
-            class="call-btn"
-            @click="onCallRequest"
-          >
-            Заказать звонок
-          </el-button>
+          <el-button class="call-btn" @click="onCallRequest"> Заказать звонок </el-button>
           <template v-if="!authStore.getToken">
-            <el-button
-              v-if="!isMobile"
-              class="auth-btn"
-              @click="isLoginVisible = true"
-            >
+            <el-button v-if="!isMobile" class="auth-btn" @click="isLoginVisible = true">
               Войти / Регистрация
             </el-button>
             <el-button
@@ -205,12 +183,7 @@ function scrollToAbout() {
             <el-icon :size="30" class="user-icon" @click="router.push({ path: '/personal' })">
               <User />
             </el-icon>
-            <el-button
-              class="auth-btn"
-              @click="onLogout"
-            >
-              Выйти
-            </el-button>
+            <el-button class="auth-btn" @click="onLogout"> Выйти </el-button>
           </template>
         </div>
       </el-header>
@@ -235,8 +208,12 @@ function scrollToAbout() {
           <template #title>Услуги</template>
           <el-sub-menu index="m1-1">
             <template #title>Мех. производство ></template>
-            <el-menu-item index="/machining" :route="{ path: '/machining' }">Токарные работы</el-menu-item>
-            <el-menu-item index="/milling" :route="{ path: '/milling' }">Фрезерные работы</el-menu-item>
+            <el-menu-item index="/machining" :route="{ path: '/machining' }"
+              >Токарные работы</el-menu-item
+            >
+            <el-menu-item index="/milling" :route="{ path: '/milling' }"
+              >Фрезерные работы</el-menu-item
+            >
             <!-- <el-menu-item index="m1-1-5" disabled>Раскрой металла / заготовительный участок</el-menu-item> -->
             <!-- <el-menu-item index="m1-1-1" disabled>Сверлильные работы</el-menu-item> -->
             <!-- <el-menu-item index="m1-1-2" disabled>Шлифовка</el-menu-item> -->
