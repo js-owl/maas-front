35591895342c0c7449c7d5b613e122fe578653b8
Alex K <aklimovskikh@yandex.ru>
2025-11-25 08:53:09 +0300

    Update 25.11

--- src/interfaces/order.interface.ts
+++ src/interfaces/order.interface.ts
@@ -1,48 +1,52 @@
 export interface IOrderBase {
-  service_id: string;
-  file_id: number;
-  quantity: number;
-  length: number;
-  width: number;
-  height: number;
-  material_id: string;
-  material_form: string;
-  tolerance_id?: string;
-  finish_id?: string;
-  cover_id: string[];
-  n_dimensions?: number;
-  k_otk: string;
-  k_cert: string[];
-  special_instructions?: string;
+  service_id: string
+  file_id: number
+  quantity: number
+  length: number
+  width: number
+  height: number
+  material_id: string
+  material_form: string
+  tolerance_id?: string
+  finish_id?: string
+  cover_id: string[]
+  n_dimensions?: number
+  k_otk: string
+  k_cert: string[]
+  special_instructions?: string
+  total_price_breakdown?: {
+    mat_price_full?: number
+    [key: string]: any
+  }
 }
 
 export interface IOrderPayload extends IOrderBase {
-  document_ids: number[];
+  document_ids: number[]
 }
 
 export interface IOrderPostPayload extends IOrderBase {
-  document_ids: number[];
+  document_ids: number[]
 }
 
 export interface IOrderResponse extends IOrderBase {
-  order_id: number;
-  user_id: number;
-  document_ids: number[];
-  composite_rig: string;
-  status: string;
-  mat_volume: number;
-  detail_price: number;
-  detail_price_one: number;
-  detail_time: number;
-  total_price: number;
-  mat_weight: number;
-  mat_price: number;
-  work_price: number;
-  k_quantity: number;
-  total_time: number;
-  created_at: string;
-  updated_at: string;
-  message: string;
-  manufacturing_cycle: number;
-  suitable_machines: [];
+  order_id: number
+  user_id: number
+  document_ids: number[]
+  composite_rig: string
+  status: string
+  mat_volume: number
+  detail_price: number
+  detail_price_one: number
+  detail_time: number
+  total_price: number
+  mat_weight: number
+  mat_price: number
+  work_price: number
+  k_quantity: number
+  total_time: number
+  created_at: string
+  updated_at: string
+  message: string
+  manufacturing_cycle: number
+  suitable_machines: []
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { createApp } from "vue";
-import router from "./router";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
 
-import "./style.css";
-import "element-plus/dist/index.css";
-import { createPinia } from "pinia";
+import './style.css'
+import 'element-plus/dist/index.css'
+import { createPinia } from 'pinia'
 
-const app = createApp(App);
+const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
+  app.component(key, component)
 }
-const pinia = createPinia();
-app.use(router).use(pinia).mount("#app");
+const pinia = createPinia()
+app.use(router).use(pinia).mount('#app')
