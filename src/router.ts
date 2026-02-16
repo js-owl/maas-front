7fc0c4cca7f379f0e415f95b7aaee64a96e96326
Alex K <aklimovskikh@yandex.ru>
2026-02-16 08:37:21 +0300

    Replace PrintingPage.vue with UslugiPrintPage.vue in router and remove PrintingPage.vue file
    - Updated the router to load UslugiPrintPage.vue instead of PrintingPage.vue for improved content organization.
    - Deleted the obsolete PrintingPage.vue file to streamline the codebase.

--- src/router.ts
+++ src/router.ts
@@ -12,7 +12,7 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: '/print',
-    component: () => import('./pages/PrintingPage.vue'),
+    component: () => import('./pages/UslugiPrintPage.vue'),
   },
   {
     path: '/painting',
