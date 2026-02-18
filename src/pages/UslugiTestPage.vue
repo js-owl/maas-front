815222aa0e9a6c6dc698346800c7b4111e6a6309
Alex K <aklimovskikh@yandex.ru>
2026-02-18 15:19:09 +0300

    Update 18.02

--- src/pages/UslugiTestPage.vue
+++ src/pages/UslugiTestPage.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import UslugiPKM from '@/components/sections/uslugi/UslugiPKM.vue'
 import UslugiExternal from '@/components/sections/uslugi/UslugiExternal.vue'
 import UslugiOptical from '@/components/sections/uslugi/UslugiOptical.vue'
 import UslugiEquipment from '@/components/sections/uslugi/UslugiEquipment.vue'
