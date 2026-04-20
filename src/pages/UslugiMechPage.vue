a3a32c6615db364e5bcf939eb0a2dd1b627999ae
Alex K <aklimovskikh@yandex.ru>
2026-04-20 13:34:26 +0300

    Update UslugiMechPage and styles for improved mobile responsiveness
    - Increased border-radius of the .uslugi-wrapper to 20px for a softer appearance.
    - Integrated HomeCalc component conditionally based on window size to enhance layout on mobile devices.
    - Utilized VueUse's useWindowSize for responsive design, ensuring better user experience across different screen sizes.

--- src/pages/UslugiMechPage.vue
+++ src/pages/UslugiMechPage.vue
@@ -1,15 +1,28 @@
 <script lang="ts" setup>
+import { computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
 import UslugiLathe from '../components/sections/uslugi/UslugiLathe.vue'
 import UslugiBending from '../components/sections/uslugi/UslugiBending.vue'
 // import UslugiLaser from '../components/sections/uslugi/UslugiLaser.vue'
 import UslugiMilling from '../components/sections/uslugi/UslugiMilling.vue'
 import UslugiTooling from '../components/sections/uslugi/UslugiTooling.vue'
 import UslugiGrinding from '../components/sections/uslugi/UslugiGrinding.vue'
+import HomeCalc from '../components/sections/HomeCalc.vue'
+
+const { width } = useWindowSize()
+const isMobile = computed(() => width.value < 768)
 </script>
 
 <template>
   <div class="main-container">
+
     <el-row>
+      <div v-if="!isMobile">
+        <el-col :offset="3" :span="18">
+          <HomeCalc />
+        </el-col>
+      </div>
+
       <UslugiLathe />
       <UslugiMilling />
       <!-- <UslugiLaser /> -->
