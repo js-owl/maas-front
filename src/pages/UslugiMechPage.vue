10d4aba303a95ae2348266064885a0a018b78ee2
Alex K <aklimovskikh@yandex.ru>
2026-04-21 08:10:37 +0300

    Update 21.04

--- src/pages/UslugiMechPage.vue
+++ src/pages/UslugiMechPage.vue
@@ -2,7 +2,7 @@
 import { computed } from 'vue'
 import { useWindowSize } from '@vueuse/core'
 import UslugiLathe from '../components/sections/uslugi/UslugiLathe.vue'
-import UslugiBending from '../components/sections/uslugi/UslugiBending.vue'
+// import UslugiBending from '../components/sections/uslugi/UslugiBending.vue'
 // import UslugiLaser from '../components/sections/uslugi/UslugiLaser.vue'
 import UslugiMilling from '../components/sections/uslugi/UslugiMilling.vue'
 import UslugiTooling from '../components/sections/uslugi/UslugiTooling.vue'
@@ -15,7 +15,6 @@ const isMobile = computed(() => width.value < 768)
 
 <template>
   <div class="main-container">
-
     <el-row>
       <div v-if="!isMobile">
         <el-col :offset="3" :span="18">
