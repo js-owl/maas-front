354d35d76748c7d0b5ae145558ee05ce2845f9ed
Alex K <aklimovskikh@yandex.ru>
2026-05-12 15:18:13 +0300

    Update 12.05

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,9 +3,13 @@ import { computed, defineAsyncComponent, onMounted, ref } from 'vue'
 import { useWindowSize } from '@vueuse/core'
 import { useCoefficientsStore } from '../stores/coefficients.store'
 const HomeUslugi = defineAsyncComponent(() => import('../components/sections/HomeUslugi.vue'))
-const HomeAdvantages = defineAsyncComponent(() => import('../components/sections/HomeAdvantages.vue'))
+const HomeAdvantages = defineAsyncComponent(
+  () => import('../components/sections/HomeAdvantages.vue')
+)
 const HomeCalc = defineAsyncComponent(() => import('../components/sections/HomeCalc.vue'))
-const HomeMilestones = defineAsyncComponent(() => import('../components/sections/HomeMilestones.vue'))
+const HomeMilestones = defineAsyncComponent(
+  () => import('../components/sections/HomeMilestones.vue')
+)
 
 const { width } = useWindowSize()
 const isMobile = computed(() => width.value < 768)
@@ -20,6 +24,7 @@ onMounted(() => {
 
 <template>
   <!-- https://www.figma.com/design/Ptb80TGgpGXXHPGVWOKhdH/MaaS?node-id=1641-91&t=wkAQmWBg5peO3d60-0 -->
+  <!-- https://www.figma.com/design/gmTpeZTQOqlccm1oIfFu2D/MaaS-%D0%9C%D0%B0%D0%BA%D0%B5%D1%82-%D0%94%D0%BB%D1%8F-Frontend?node-id=4183-189 -->
   <div style="background-color: var(--bgcolor)">
     <div v-if="!isMobile">
       <el-row>
