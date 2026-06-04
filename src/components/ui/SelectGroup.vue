c8ec59e611447e6a3c531d7f39028d24c86d2000
Alex K <aklimovskikh@yandex.ru>
2026-06-04 14:11:18 +0300

    Enhance SelectGroup component styling and structure
    - Wrapped the el-select-v2 component in a div for improved layout.
    - Added a full-width class to the select component for better responsiveness.
    - Introduced scoped styles to customize the appearance of the select component, including background color, border radius, and font styles.
    - Enhanced placeholder and selected item styles for improved readability.
    
    These changes improve the visual consistency and user experience of the SelectGroup component.

--- src/components/ui/SelectGroup.vue
+++ src/components/ui/SelectGroup.vue
@@ -30,12 +30,54 @@ const model = computed({
 </script>
 
 <template>
-  <el-select-v2
-    v-model="model"
-    :filterable="filterable"
-    :options="options"
-    :placeholder="placeholder"
-    :style="{ width }"
-  />
+  <div>
+    <el-select-v2
+      v-model="model"
+      :filterable="filterable"
+      :options="options"
+      :placeholder="placeholder"
+      size="large"
+      class="full"
+      :style="{ width }"
+    />
+  </div>
 </template>
+
+<style scoped>
+.full {
+  width: 100%;
+}
+
+:deep(.el-select__wrapper) {
+  background-color: var(--whity);
+  border: 0;
+  border-radius: 10px;
+  min-height: 48px;
+  box-shadow: none;
+  font-size: 20px;
+  font-weight: 600;
+  color: black;
+  line-height: normal;
+  padding: 12px 24px;
+}
+
+:deep(.el-select__placeholder),
+:deep(.el-select__selected-item) {
+  color: black;
+  font-family: 'Montserrat-Medium', sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+}
+
+:deep(.el-select__caret) {
+  color: var(--button);
+  font-size: 20px;
+}
+
+:deep(.el-select:hover .el-select__wrapper),
+:deep(.el-select .el-select__wrapper.is-focused),
+:deep(.el-select .el-select__wrapper.is-hovering) {
+  box-shadow: none;
+}
+</style>
 
